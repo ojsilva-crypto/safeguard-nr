54bafbf55aae22a6f9eb50ee4deb9d2c8fb0424f
ojsilva-crypto <ojsilva@gmail.com>
2026-05-23 19:20:53 -0300

    Set up routing and authentication in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,36 @@
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import { PrivateRoute } from './components/PrivateRoute'
+import { Login } from './pages/Login'
+import { Dashboard } from './pages/Dashboard'
+import { Clientes } from './pages/Clientes'
+import { Produtos } from './pages/Produtos'
+import { Pedidos } from './pages/Pedidos'
+import { Layout } from './components/Layout'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Layout />
+              </PrivateRoute>
+            }
+          >
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="clientes" element={<Clientes />} />
+            <Route path="produtos" element={<Produtos />} />
+            <Route path="pedidos" element={<Pedidos />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
