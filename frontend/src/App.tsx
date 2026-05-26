43eec81ffa247be64aceafeeeb5227a32c27370a
ojsilva-crypto <ojsilva@gmail.com>
2026-05-26 18:16:24 -0300

    Refactor App component routing and add Toaster
    Refactor routing by replacing Routes with AppRoutes and adding Toaster component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import { PrivateRoute } from './components/PrivateRoute'
-import { Layout } from './components/Layout'
-import { Login } from './pages/Login'
-import { Dashboard } from './pages/Dashboard'
-import { Riscos } from './pages/Riscos'
-import { Pgr } from './pages/Pgr'
-import { Colaboradores } from './pages/Colaboradores'
-import { Compliance } from './pages/Compliance'
+import AppRoutes from './routes'
+import { Toaster } from 'react-hot-toast'
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          {/* Rota pública */}
-          <Route path="/login" element={<Login />} />
-
-          {/* Rotas privadas */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Layout />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<Navigate to="/dashboard" replace />} />
-            <Route path="dashboard"      element={<Dashboard />} />
-            <Route path="riscos"         element={<Riscos />} />
-            <Route path="pgr"            element={<Pgr />} />
-            <Route path="colaboradores"  element={<Colaboradores />} />
-            <Route path="compliance"     element={<Compliance />} />
-          </Route>
-
-          {/* Fallback */}
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
+        <AppRoutes />
+        <Toaster position="top-right" />
       </AuthProvider>
     </BrowserRouter>
   )
 }
+
+export default App
