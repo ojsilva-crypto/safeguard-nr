4161140bc86263a8163810e6f78caed944108967
ojsilva-crypto <ojsilva@gmail.com>
2026-05-26 18:18:37 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Toaster } from 'react-hot-toast'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster
-      position="top-right"
-      toastOptions={{
-        duration: 4000,
-        style: {
-          background: '#1f2937',
-          color: '#f9fafb',
-          borderRadius: '10px',
-          fontSize: '14px',
-        },
-        success: {
-          iconTheme: { primary: '#22c55e', secondary: '#fff' },
-        },
-        error: {
-          iconTheme: { primary: '#ef4444', secondary: '#fff' },
-        },
-      }}
-    />
   </React.StrictMode>,
 )
